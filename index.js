73c824680f2b6c2e2ab88b1b3376fe0a7ea18f20
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-03 21:41:08 +0600

    Create node server, conncet it with mongodb and insert data

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Use middleware 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Running my node crud server');
+});
+
+app.listen(port, () => {
+    console.log('Crud server is running');
+})
+
+
+// For mongoDb connection:
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://dbuser1:qWDimG0ngJsC64iR@cluster0.fq0uj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user");
+        const user = { name: 'Monisha', email: 'monisha@gmail.com' };
+        const result = await userCollection.insertOne(user);
+        console.log(`User inserted with id: ${result.insertedId}`);
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
